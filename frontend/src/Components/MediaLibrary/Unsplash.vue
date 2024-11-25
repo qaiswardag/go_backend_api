9a5c6bde9a1857204638685f12541db8b02ecba3
Qais Wardag <qais.wardag@outlook.com>
2024-11-25 13:38:27 +0100

    Remove Unsplash API key from .env and update error handling in Unsplash component

--- frontend/src/Components/MediaLibrary/Unsplash.vue
+++ frontend/src/Components/MediaLibrary/Unsplash.vue
@@ -126,14 +126,11 @@ onMounted(() => {
         getUnsplashImages &&
         !getUnsplashImages.isLoading &&
         getUnsplashImages.isError &&
-        getUnsplashImages.isSuccess
+        !getUnsplashImages.isSuccess
       "
     >
       <p class="myPrimaryParagraphError">{{ getUnsplashImages.error }}</p>
     </div>
-    <p class="myPrimaryParagraph my-6 text-myPrimaryErrorColor">
-      {{ getUnsplashImages && getUnsplashImages.error }}
-    </p>
     <div
       v-if="
         getUnsplashImages &&
