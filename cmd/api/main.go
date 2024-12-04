fdee5d85c65b67f7b4e5af708d3f2da26d6ff830
qaiswardag <qais.wardag@outlook.com>
2024-12-04 11:09:27 +0100

    Add comments for server configuration and traffic management

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,18 @@ import (
 func main() {
 	// Load environment variables file
 	config.LoadEnvironmentFile()
+
+	// Go Server Ports Behind a Web Serve:
+	// Do not directly expose the Go port (e.g., localhost:8080) to the public internet.
+
+	// Setting Up the Server to Forward Requests to Go Application:
+	// Hosting company: configure the server to forwards public requests from (e.g., www.myissue.digitalocean.com) to (e.g., localhost:8080).
+	// Ensure that the Go application is running on the specific internal port (e.g., localhost:8080) and listens for these requests.
+
+	// Managing High Traffic with Go:
+	// With millions of requests, distributing the load ensures that no single instance is overwhelmed, improving performance and reliability.
+	// Ensure that the Go application is deployed across multiple instances, and
+	// use load balancing to distribute requests evenly across those instances.
 	serverAddr := config.GetEnvironmentVariable("SERVER_ADDR")
 
 	handler := routes.MainRouter()
