45e7ecd70b7bbff6af182aace104249217a586dc
qaiswardag <qais.wardag@outlook.com>
2024-12-11 15:27:59 +0100

    Refactor User model: clean up code structure and add Public and CurrentTeam fields

--- internal/model/user.go
+++ internal/model/user.go
@@ -7,17 +7,11 @@ type User struct {
 	UserName string `gorm:"unique"`
 	Email    string `gorm:"unique;not null"`
 	// Hide the password from the JSON response by omitting the field using the json:"-"`
-	Password      string `gorm:"not null" json:"-"`
+	Password string `gorm:"not null" json:"-"`
+
 	FirstName     string `gorm:"not null"`
 	LastName      string `gorm:"not null"`
 	LoginAttempts *int
-}
-
-func UserObject() map[string]interface{} {
-	return map[string]interface{}{
-		"user": map[string]string{
-			"firstName": "John",
-			"lastName":  "Doe",
-		},
-	}
+	Public        *bool `gorm:""`
+	CurrentTeam   *int  `gorm:""`
 }
